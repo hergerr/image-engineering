6d0e34383323a9ea3f8e4fa9c4db18e8d65413c3
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-04-19 15:22:28 +0200

    Got png

--- lab4/lab4-png.py
+++ lab4/lab4-png.py
@@ -4,25 +4,32 @@ import struct
 import zlib
 
 # Image data
-image = numpy.array([[[255,   0,   0], [  0, 255,  0]],
-                     [[  0,   0, 255], [ 55,  55,  0]]],
-                    dtype=numpy.uint8)  # TODO: implement
+image = numpy.array([[[255,   0,   0], [0, 255,  0]],
+                     [[0,   0, 255], [55,  55,  0]]],
+                    dtype=numpy.uint8)
 
 # Construct signature
-png_file_signature = b"\x89" + "PNG\r\n\x1A\n".encode('ascii')  # TODO: implement
+png_file_signature = b"\x89" + \
+    "PNG\r\n\x1A\n".encode('ascii')  # TODO: implement
+h, w, _ = image.shape
 
 # Construct header
-header_id = 'IDHR'  # TODO: implement
-header_content = ''  # TODO: implement
-header_size = ''  # TODO: implement
-header_crc = ''  # TODO: implement
+# ! big endian, I - 4b unsigned int, B 1B unsigned char 
+header_id = b'IHDR'
+header_content = struct.pack('!IIBBBBB', w, h, 8, 2, 0, 0, 0)
+header_size = struct.pack('!I', len(header_content))    # it means unsigned int
+header_crc = struct.pack('!I', zlib.crc32(header_id + header_content))
 png_file_header = header_size + header_id + header_content + header_crc
 
 # Construct data
-data_id = ''  # TODO: implement
-data_content = ''  # TODO: implement
-data_size = ''  # TODO: implement
-data_crc = ''  # TODO: implement
+data_id = b'IDAT'
+idat = numpy.empty((h, w*3 + 1), dtype=numpy.uint8)
+idat[:, 1:] = image.reshape(h, w*3)
+idat[:, 0] = 0
+
+data_content = zlib.compress(idat, 9)
+data_size = struct.pack('!I', len(data_content))
+data_crc = struct.pack('!I', zlib.crc32(data_id + data_content))
 png_file_data = data_size + data_id + data_content + data_crc
 
 # Consruct end
